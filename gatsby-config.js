7e08d96f9b8621ba68450e842575132f88124e38
Cooper Garren <cgarren18@icloud.com>
2022-06-29 12:32:22 -0400

    Dependency updates and added chart.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,12 @@ module.exports = {
       title: `Spotifydata`,
     siteUrl: `https://www.spotifydata.com`
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
