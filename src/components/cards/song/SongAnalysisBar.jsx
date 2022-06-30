7a3f33ff3f95a2bdac9f4bb41e3b7b863b85bd93
Cooper Garren <cgarren18@icloud.com>
2022-06-29 20:08:29 -0400

    total Analysis Bar revamp

--- src/components/cards/song/SongAnalysisBar.jsx
+++ src/components/cards/song/SongAnalysisBar.jsx
@@ -1,49 +1,137 @@
-import React from "react";
-import { ProgressBar } from "react-bootstrap";
-import { barStyle } from "./SongAnalysisBar.module.css";
+import React, { useEffect, useRef, useState } from "react";
+import { Col, Row, Container, Tooltip, Overlay } from "react-bootstrap";
+import {
+  pickHex,
+  decimalToInteger,
+  tempoCalc,
+  loudnessCalc,
+  modeCalc,
+  keyCalc,
+  timeSignatureCalc,
+  durationCalc,
+} from "../../../utilities/helpers";
+import { barStyle, slider } from "./SongAnalysisBar.module.css";
 
-const SongAnalysisBar = ({ sections, attribute, track_duration }) => {
-  // useEffect(() => {
-  //   // make sure the element exists (might not be loaded yet)
-  //   if (ref.current) {
-  //     // get the inner progress bar (the colorful one)
-  //     const inner = ref.current.querySelector(".progress-bar");
-  //     if (inner) {
-  //       // if the inner bar exists, set its color
-  //       inner.style.backgroundColor = color;
-  //     }
-  //   }
-  // }, [color, ref.current]);
+const SongAnalysisBar = ({ sections, track_duration }) => {
+  const [value, setValue] = useState(0);
+  const [tempo, setTempo] = useState(0);
+  const [loudness, setLoudness] = useState(0);
+  const [mode, setMode] = useState(0);
+  const [key, setKey] = useState(0);
+  const [timeSignature, setTimeSignature] = useState(0);
+  const [attribute, setAttribute] = useState("tempo");
 
-  // useEffect(() => {
-  //   // make sure the element exists (might not be loaded yet)
-  //   if (ref.current) {
-  //     // get the inner progress bar (the colorful one)
-  //     const inner = ref.current.querySelector(".progress-bar");
-  //     if (inner) {
-  //       // if the inner bar exists, set its height
-  //       inner.style.height = barHeight;
-  //     }
-  //   }
-  // }, [barHeight, ref.current]);
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
 
-  //let decimal = value / duration;
-  const variants = ["info", "warning", "danger", "primary", "success"];
+  function showTooltip() {
+    setShow(true);
+  }
+
+  function hideTooltip() {
+    setShow(false);
+  }
+
+  function buildColorAndPercentageString() {
+    let str = "";
+    let prevPercentage = 0;
+    let minTempo = 1000;
+    let maxTempo = 0;
+    for (let section of sections) {
+      if (section.tempo > maxTempo) {
+        maxTempo = section.tempo;
+      }
+      if (section.tempo < minTempo) {
+        minTempo = section.tempo;
+      }
+    }
+    let spread = maxTempo - minTempo;
+
+    for (let section of sections) {
+      let percentage = decimalToInteger(section.duration / track_duration);
+      let newPrecentage = prevPercentage + percentage;
+      //For tempo
+      let decimal = (section.tempo - minTempo) / spread;
+      let pickedColor = pickHex([246, 45, 45], [16, 52, 166], decimal);
+      let color =
+        "rgb(" +
+        pickedColor[0] +
+        ", " +
+        pickedColor[1] +
+        ", " +
+        pickedColor[2] +
+        ")";
+      str =
+        str + ", " + color + " " + prevPercentage + "% " + newPrecentage + "%";
+      prevPercentage += percentage;
+    }
+    return str;
+  }
+
+  useEffect(() => {
+    if (target.current) {
+      const slider = target.current;
+      if (slider) {
+        slider.style.backgroundImage =
+          "linear-gradient(to right" + buildColorAndPercentageString() + ")";
+        slider.max = track_duration;
+      }
+    }
+  }, [sections, target.current]);
+
+  useEffect(() => {
+    for (let section of sections) {
+      if (value < section.start + section.duration) {
+        console.log("setting");
+        setTempo(tempoCalc(section.tempo));
+        setLoudness(loudnessCalc(section.loudness));
+        setMode(modeCalc(section.mode));
+        setKey(keyCalc(section.key));
+        setTimeSignature(timeSignatureCalc(section.time_signature));
+        break;
+      }
+    }
+  }, [value, sections]);
+
+  function updateValue(event) {
+    setValue(event.target.value);
+  }
+
+  // {sections.map((item, index) => {
+  //           variant={variants[index]}
+  //           now={item.duration}
+  //           label={item[attribute]}
+  //           key={index}
+  //           min={0}
+  //           max={track_duration}
+  //     })}
   return (
-    <ProgressBar className={barStyle}>
-      {sections.map((item, index) => {
-        return (
-          <ProgressBar
-            variant={variants[index]}
-            now={item.duration}
-            label={item[attribute]}
-            key={index}
-            min={0}
-            max={track_duration}
-          />
-        );
-      })}
-    </ProgressBar>
+    <div>
+      <input
+        type="range"
+        ref={target}
+        className={slider}
+        min={0}
+        value={value}
+        onChange={updateValue}
+        onMouseDown={showTooltip}
+        onMouseUp={hideTooltip}
+        onTouchStart={showTooltip}
+        onTouchEnd={hideTooltip}
+      />
+      <Overlay target={target.current} show={show} placement="top">
+        {(props) => <Tooltip {...props}>{durationCalc(value * 1000)}</Tooltip>}
+      </Overlay>
+      <Container>
+        <Row>
+          <Col>Tempo: {tempo} BPM</Col>
+          <Col>Loudness: {loudness} dB</Col>
+          <Col>Mode: {mode}</Col>
+          <Col>Key: {key}</Col>
+          <Col>Time Signature: {timeSignature}</Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
--- src/components/cards/song/SongAnalysisWidget.jsx
+++ src/components/cards/song/SongAnalysisWidget.jsx
@@ -10,36 +10,9 @@ import {
 //TODO: Get creative with how you display this on mobile. Maybe a circular thing or vertical bar? Maybe a waterfall type diagram
 
 const SongAnalysisWidget = ({ sections, duration }) => {
-  const [selection, setSelection] = useState("tempo");
-  function changedTab(eventKey, event) {
-    setSelection(eventKey);
-  }
-
   return (
     <div className={"p-3 " + widgetStyle}>
-      <Tabs
-        defaultActiveKey="tempo"
-        id="uncontrolled-tab-example"
-        className={"mb-3 " + tabsStyle}
-        variant="pills"
-        onSelect={changedTab}
-        justify
-      >
-        <Tab eventKey="tempo" title="Tempo" tabClassName={tabStyle}></Tab>
-        <Tab eventKey="loudness" title="Loudness" tabClassName={tabStyle}></Tab>
-        <Tab eventKey="mode" title="Mode" tabClassName={tabStyle}></Tab>
-        <Tab eventKey="key" title="Key" tabClassName={tabStyle}></Tab>
-        <Tab
-          eventKey="time_signature"
-          title="Time Signature"
-          tabClassName={tabStyle}
-        ></Tab>
-      </Tabs>
-      <SongAnalysisBar
-        sections={sections}
-        attribute={selection}
-        track_duration={duration}
-      />
+      <SongAnalysisBar sections={sections} track_duration={duration} />
     </div>
   );
 };
